ec4d7e515d013d017094b6fdcc594fee6192a5e8
peter dicicco <peter.dicicco@hotmail.com>
2026-07-09 08:00:30 -0400

    eds book and rb2

--- scripts/obj/Debug/net10.0/scripts.AssemblyInfo.cs
+++ scripts/obj/Debug/net10.0/scripts.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("scripts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ae20c76c435d5331e97cbaa912c1d7a71615915")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+067813072ce2021d3e1e8d9d252ead3b9ce50546")]
 [assembly: System.Reflection.AssemblyProductAttribute("scripts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("scripts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
